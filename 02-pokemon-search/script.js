c579bc748ead71bfb08c0819cccfa8bf56415f9a
kunxin-chor <chorkunxin@yahoo.com>
2022-05-30 03:59:08 +0000

    Added pokeapi example

---
+++ 02-pokemon-search/script.js
@@ -0,0 +1,18 @@
+const BASE_API_URL = "https://pokeapi.co/api/v2/"
+
+document.querySelector("#btnSearch")
+    .addEventListener('click', async function(){
+        let pokemon = document.querySelector("#txtPokemon").value;
+        let url = `${BASE_API_URL}pokemon/${pokemon}`;
+        let response = await axios.get(url);
+        console.log(response.data);
+        document.querySelector('#result')
+            .innerHTML = `
+                <h1>${response.data.name}</h1>
+                <img src="${response.data.sprites.other["official-artwork"].front_default}">
+                <ul>
+                    <li>Base experience: ${response.data.base_experience}</li>
+                    <li>Weight: ${response.data.weight}</li> 
+                </ul>
+            `
+    })
